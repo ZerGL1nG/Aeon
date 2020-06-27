ff76bd85235c26becf0c7fa47154b77b644d4f92
AceSpade <313121@mail.ru>
2020-06-27 22:05:26 +0500

    Initial commit

---
+++ Aeon/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks;
+using System.Xml;
+using AI.NeuralNetwork;
+using AI.NeuralNetwork.Algs;
+
+namespace TanksGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+    }
+}
